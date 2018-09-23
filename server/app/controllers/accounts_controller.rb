7ddd15a4d0bd46a12eba73ee513ec6c393cd6044
Rafael Eiki <rafaeleiki@yahoo.com.br>
2018-09-23 12:52:19 -0300

    Allows CRUD API to work

--- server/app/controllers/accounts_controller.rb
+++ server/app/controllers/accounts_controller.rb
@@ -69,6 +69,6 @@ class AccountsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def account_params
-      params.require(:account).permit(:user, :email, :nickname, :password_digest, :last_login)
+      params.require(:account).permit(:user, :email, :nickname, :password, :last_login)
     end
 end
