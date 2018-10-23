f6d1e92220b7701e808e436f6717b81e32caa7b4
Rafael Eiki <rafaeleiki@yahoo.com.br>
2018-10-22 21:13:23 -0300

    Changes character to a numeric ID

--- server/db/migrate/20180923145158_create_game_accounts.rb
+++ server/db/migrate/20180923145158_create_game_accounts.rb
@@ -2,7 +2,7 @@ class CreateGameAccounts < ActiveRecord::Migration[5.2]
   def change
     create_table :game_accounts do |t|
       t.references :account, foreign_key: true
-      t.references :character, foreign_key: true
+      t.integer :character_id, default: 0
       t.integer :matches_played, default: 0
       t.integer :winning_matches, default: 0
 
